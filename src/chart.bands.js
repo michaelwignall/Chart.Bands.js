c2cb1f69305e51e438bb65004e34f63ace2d9e81
Mike Wignall <michael@born-digital.co.uk>
2017-10-26 19:28:19 +0100

    Multiple bands working

--- src/chart.bands.js
+++ src/chart.bands.js
@@ -20,6 +20,8 @@ Chart.Bands = Chart.Bands || {};
 
 defaultOptions = Chart.Bands.defaults = {
     bands: {
+        from: 0,
+        to: 0,
         yValue: false,
         bandLine: {
             stroke: 0.01,
@@ -30,7 +32,7 @@ defaultOptions = Chart.Bands.defaults = {
             fontFamily: 'Helvetica Neue, Helvetica, Arial, sans-serif',
             fontStyle: 'normal'
         },
-        belowThresholdColour: [
+        color: [
             'rgba(0, 255, 0, 1.000)'
         ]
     }
@@ -84,25 +86,54 @@ function addBandLineLabel (ctx, options, position) {
 }
 
 function pluginBandOptionsHaveBeenSet (bandOptions) {
-    return (typeof bandOptions.belowThresholdColour === 'object' && bandOptions.belowThresholdColour.length > 0 && typeof bandOptions.yValue === 'number');
+    return (typeof bandOptions.color === 'object' && bandOptions.color.length > 0 && typeof bandOptions.yValue === 'number');
 }
 
-function calculateGradientFill (ctx, scale, height, baseColor, gradientColor, value) {
-    var yPos = scale.getPixelForValue(value),
-        grd = ctx.createLinearGradient(0, height, 0, 0),
-        gradientStop = 1 - (yPos / height);
+function calculateGradientFill (ctx, scale, height, baseColor, gradientColor, value, bands) {
+    // In here we want to figure out all of the stops along the way and then add them to the line
+    var colours = ['red', 'green', 'blue'];
 
+    // figure out the position of all the stops
+    var stops = [];
+
+    for (var i = 0; i < bands.length; i++) {
+        var band = bands[i];
+        var colour = colours[i];
+        var fromY = scale.getPixelForValue(band.from);
+        var toY = scale.getPixelForValue(band.to);
+        var fromStop = 1 - (fromY / height);
+        var toStop = 1 - (toY / height);
+
+        stops.push({
+            pos: fromStop,
+            colour: colour
+        });
+
+        stops.push({
+            pos: toStop,
+            colour: colour
+        });
+    }
+    window.stops = stops;
+
+    // add stops to the gradient
+    var error = false;
     try {
-        grd.addColorStop(0, gradientColor);
-        grd.addColorStop(gradientStop, gradientColor);
-        grd.addColorStop(gradientStop, baseColor);
+        var grd = ctx.createLinearGradient(0, height, 0, 0);
+        
+        for (var i = 0; i < stops.length; i++) {
+            var stop = stops[i];
+            grd.addColorStop(stop.pos, stop.colour);
+        }
+
         grd.addColorStop(1.00, baseColor);
 
-        return grd;
     } catch (e) {
         console.warn('ConfigError: Chart.Bands.js had a problem applying one or more colors please check that you have selected valid color strings');
-        return baseColor;
+        error = true;
     }
+
+    return error ? baseColor : grd;
 }
 
 function isPluginSupported (type) {
@@ -141,13 +172,15 @@ var BandsPlugin = Chart.PluginBase.extend({
             if (pluginBandOptionsHaveBeenSet(bandOptions)) {
 
                 for (var i = 0; i < chartInstance.chart.config.data.datasets.length; i++) {
+                    // MIKE: This will be done too many times as we're looping over shizzle, lets refactor once things are working though
                     fill = calculateGradientFill(
                                             node.getContext("2d"),
                                             chartInstance.scales['y-axis-0'],
                                             chartInstance.chart.height,
                                             baseColor[i],
-                                            bandOptions.belowThresholdColour[i],
-                                            bandOptions.yValue
+                                            bandOptions.color[i],
+                                            bandOptions.yValue,
+                                            chartInstance.options.bands
                                         );
                     chartInstance.chart.config.data.datasets[i][colourProfile] = fill;
                 }
